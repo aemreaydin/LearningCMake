90a9d98e1c861091e2cc77a0766263e8c80e8293
aemreaydin <aeaydin1@gmail.com>
2020-05-26 20:58:29 +0300

    Added two libs as a separate project, they are added using find_package in the main project, an environment variable needs to be set so that libs are easier to find

--- cmake/FindMathLib.cmake
+++ cmake/FindMathLib.cmake
@@ -1,3 +1,5 @@
+set(MathLib_INCLUDE_DIR $ENV{BaseLibs}/include)
+set(MathLib_LIBRARY_DIR $ENV{BaseLibs}/lib/MathLib)
 find_package(MathLib REQUIRED)
 if(MathLib_FOUND)
     message(STATUS "MathLib found.")
